ccb6286ca8e20ef787d63352c739ab56dc61ff20
ernane <ernanecarvalho89@gmail.com>
2021-07-23 04:50:47 -0300

    Initial commit of exercise lecture Class and Method Abstract

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractMethodFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
